0d57410c68dc44efd7f512d0bc43309dec0b3d5c
iliaska69 <ilia08359@gmail.com>
2022-02-19 12:34:16 +0300

    Server
    added server

---
+++ cwSpring/src/main/java/com/cw/cwSpring/CwSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cw.cwSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwSpringApplication.class, args);
+	}
+
+}
