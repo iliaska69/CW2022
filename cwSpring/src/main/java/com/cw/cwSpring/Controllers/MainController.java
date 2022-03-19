67930d1b7c5e547158bf1df4389446c476f68ea1
iliaska69 <ilia08359@gmail.com>
2022-03-19 13:38:49 +0300

    dbRegistration

--- cwSpring/src/main/java/com/cw/cwSpring/Controllers/MainController.java
+++ cwSpring/src/main/java/com/cw/cwSpring/Controllers/MainController.java
@@ -1,11 +1,19 @@
 package com.cw.cwSpring.Controllers;
 
+import com.cw.cwSpring.models.User;
+import com.cw.cwSpring.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/authorization")
     public String authorization(Model model) {
@@ -17,5 +25,10 @@ public class MainController {
         //model.addAttribute("name", "Hello!");
         return "registration";
     }
-
+    @PostMapping("/registration")
+    public String registrationForm(@RequestParam String login, @RequestParam String password,@RequestParam String passwordSecond,Model model) {
+        User user = new User(login,password);
+        userRepository.save(user);
+        return "authorization";
+    }
 }
---
+++ cwSpring/src/main/java/com/cw/cwSpring/models/User.java
@@ -0,0 +1,56 @@
+package com.cw.cwSpring.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String login;
+    private String password;
+    private Boolean isTrust = false;
+
+    public User() {
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getTrust() {
+        return isTrust;
+    }
+
+    public void setTrust(Boolean trust) {
+        isTrust = trust;
+    }
+}
