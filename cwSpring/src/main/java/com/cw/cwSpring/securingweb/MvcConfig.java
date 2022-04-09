3024313c2388bfe44d130ee0ee7974bcf9f52f1e
iliaska69 <34137719+iliaska69@users.noreply.github.com>
2022-04-09 13:48:14 +0300

    css header

--- cwSpring/src/main/java/com/cw/cwSpring/securingweb/MvcConfig.java
+++ cwSpring/src/main/java/com/cw/cwSpring/securingweb/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.cw.cwSpring.securingweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/assets/**")) {
+            registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+        }
     }
 
 }
